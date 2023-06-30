346255b3e2c6d68fbe65a85fcf2e689a005d41be
PHadaia <philipp.hadaia@outlook.de>
2023-07-01 00:51:29 +0200

    Implement additional test cases for PricingServiceApplication

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,6 +1,11 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.service.PriceException;
+import com.udacity.pricing.service.PricingService;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,6 +16,27 @@ public class PricingServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testGetPrice() throws PriceException {
+		// Arrange
+		Long testId = 10L;
+
+		// Act
+		Price actualPrice = PricingService.getPrice(testId);
+
+		// Assert
+		Assertions.assertTrue(actualPrice.getPrice().doubleValue() > 0.0);
+	}
+
+	@Test
+	public void testGetPriceInvalidId() throws PriceException {
+		// Arrange
+		Long testId = 200L;
+
+		// Act & Assert
+		Assertions.assertThrows(PriceException.class, () -> PricingService.getPrice(testId));
 	}
 
 }
